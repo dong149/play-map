28cd04d3372f51a4802293c26cf77aaa018222de
DANNIEL <donghoon149@gmail.com>
2020-05-02 21:33:15 +0900

    add kakao map api

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+const { kakao } = window;
+class PopUp extends Component {
+  constructor(props) {
+    super(props);
+  }
+  map;
+  markers = [];
+  infowindows = [];
+  componentDidMount() {
+    var container = document.getElementById("myMap"); //지도를 담을 영역의 DOM 레퍼런스
+    var options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(35.157588, 129.058822), //지도의 중심좌표.
+      level: 4, //지도의 레벨(확대, 축소 정도)
+    };
+    this.map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    console.log(this.map);
+  }
+  render() {
+    return (
+      <div className="popup">
+        <div id="myMap" style={{ width: "500px", height: "400px" }} />
+      </div>
+    );
+  }
+}
 
 function App() {
-  const [data, setData] = useState({});
-  fetch("/data")
-    .then((res) => res.json())
-    .then((data) => setData(data));
+  // const [data, setData] = useState({});
+  // fetch("/data")
+  //   .then((res) => res.json())
+  //   .then((data) => setData(data));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {data && <p>{data.lastname}</p>}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PopUp />
     </div>
   );
 }
