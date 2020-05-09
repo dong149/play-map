ed1b80be841f1fe452285875ee325bf70827cf49
DANNIEL <donghoon149@gmail.com>
2020-05-09 22:00:02 +0900

    add pc room api

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class PopUp extends Component {
     var container = document.getElementById("myMap"); //지도를 담을 영역의 DOM 레퍼런스
     var options = {
       //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(35.157588, 129.058822), //지도의 중심좌표.
+      center: new kakao.maps.LatLng(37.6345882413846, 126.8327565232912), //지도의 중심좌표.
       level: 4, //지도의 레벨(확대, 축소 정도)
     };
     this.map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
